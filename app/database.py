bdb90b3adbbd89f485c7c0e1eb166534b15fc5db
OmkarBakare17 <omkarbakare7@gmail.com>
2025-06-01 15:24:53 +0530

    db changes

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://username:password@localhost:5432/mydb"
+DATABASE_URL = "postgresql+asyncpg://test123:test123@host.docker.internal:5432/mydb"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
